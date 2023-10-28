85f3ff307012f0d9bdd4873d3a05169c15ec0213
Jaiswal <ajaiswal@comscore.com>
2023-10-29 02:43:05 +0530

    added budget manager chrome extension

---
+++ Budget Manager/popup.js
@@ -0,0 +1,26 @@
+const submitBtn = document.getElementById('submit');
+const amountInput=document.getElementById('amount');
+const totalSpendH2 = document.getElementById('total');
+
+submitBtn.addEventListener('click', (e) =>{
+    chrome.storage.sync.get('total_result').then((result) => {
+        let newTotal = 0;
+        if(result.total_result){
+            newTotal = newTotal + parseInt(result.total_result);
+        }
+        let amount = amountInput.value;
+        if(amount) {
+            newTotal = newTotal + parseInt(amount);
+        }
+
+        totalSpendH2.innerText = newTotal;
+        amountInput.value = '';
+
+      chrome.storage.sync.set({ 'total_result': newTotal });
+      });
+      
+})
+
+chrome.storage.sync.get('total_result').then((result)=>{
+    totalSpendH2.innerText = result.total_result;
+});
