31bdbf18599604cadbfcf54c5e002b9eb81318d7
Jaiswal <ajaiswal@comscore.com>
2023-11-06 00:51:53 +0530

    handle how to pass multipe values from popupJS to contentJS

--- Connection from PopupJS to ContentScript and Background/content.js
+++ Connection from PopupJS to ContentScript and Background/content.js
@@ -2,6 +2,20 @@ console.log("HELLO FROM CONTENT SCRIPT");
 
 // how we receive data from popup.js
 chrome.runtime.onMessage.addListener(function (request, sender, sendresponse) {
-  console.log("content script---", request);
-  html2pdf(document.body);
+  var element = document.body;
+  var opt = {
+    margin: 0.25,
+    filename: "myfile.pdf",
+    image: { type: "jpeg", quality: 1 },
+    enableLinks: true,
+    html2canvas: { scale: 2 },
+    jsPDF: {
+      unit: "in",
+      format: request.message.pageSizeValue,
+      orientation: request.message.orientValue,
+    },
+  };
+
+  // New Promise-based usage:
+  html2pdf().set(opt).from(element).save();
 });
--- Connection from PopupJS to ContentScript and Background/popup.js
+++ Connection from PopupJS to ContentScript and Background/popup.js
@@ -1,12 +1,30 @@
 let convertBtn = document.getElementById("convert-btn");
+let pageSize = document.getElementById('page-size');
+let orient = document.getElementById('orientation');
+let pageSizeValue = pageSize.value;
+let orientValue = orient.value;
+
+
 convertBtn.addEventListener("click", (e) => {
   // how we send data to background.js
   chrome.runtime.sendMessage({ greeting: "hello" });
 });
 
+pageSize.addEventListener("change", (e) =>{
+  pageSizeValue= e.target.value;
+});
+
+orient.addEventListener("change", (e)=>{
+  orientValue = e.target.value;
+})
+
 convertBtn.addEventListener("click", (e) => {
   // how we send data to content.js
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { message: e.target.innerText });
+    let params = {
+      pageSizeValue,
+      orientValue
+    }
+    chrome.tabs.sendMessage(tabs[0].id, { message: params });
   });
 });
