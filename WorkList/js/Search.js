eb5af213e12de38ea8bac26c29cfcdf63e63e7d4
Jaiswal <ajaiswal@comscore.com>
2023-11-24 02:55:54 +0530

    added WorkList

---
+++ WorkList/js/Search.js
@@ -0,0 +1,12 @@
+const search = document.querySelector(".js-form");
+
+search.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const value = document.querySelector(".inputValue").value;
+  console.log("encodeURIComponent(value)", encodeURIComponent(value));
+  if (encodeURIComponent(value) != "") {
+    window.location.href = `https://www.google.com/search?q=${encodeURIComponent(
+      value
+    )}`;
+  } else return false;
+});
